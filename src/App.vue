8b756060b2a489f89960a91384dcd42039c42b6a
nargyanti <nargyanti@gmail.com>
2024-04-01 14:40:11 +0700

    feat: create under construction page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <!-- Header -->
   <RouterView />
 </template>
