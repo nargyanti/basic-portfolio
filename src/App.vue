a011f17688c4ff9a15eec933ac41c6cad2a0d8d3
nargyanti <nargyanti@gmail.com>
2024-04-21 20:50:02 +0700

    refactor: change page's base

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,8 @@ import Header from './components/Header.vue';
 </script>
 
 <template>
-  <div class="bg-white pb-6 sm:pb-8 lg:pb-12">
-    <div class="mx-auto max-w-screen-2xl px-8 md:px-12">
-      <Header></Header>
-      <RouterView />
-    </div>
+  <div class="mx-auto max-w-screen-lg px-6 bg-light">
+    <Header></Header>
+    <RouterView />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#176B87',
+        accent: '#E8F0F3',
+        light: '#FDFEFF'
+      }
+    }
   },
   plugins: []
 }
