62567e543f3e759bd953c8fb97abb9c17ad621d3
nargyanti <nargyanti@gmail.com>
2024-06-01 18:32:45 +0700

    Apply prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#176B87',
-        accent: '#E8F0F3',
-        light: '#FDFEFF'
-      }
-    }
-  },
-  plugins: []
-}
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#378E87",
+                "primary-dark": "#00635B",
+                secondary: "#DCF3EF",
+                light: "#FDFEFF",
+            },
+        },
+    },
+    plugins: [],
+};
