e5bba1e940d1b6dcc9c8a6b9813e7aa09d6923a8
nargyanti <nargyanti@gmail.com>
2024-06-01 21:45:38 +0700

    Delete /projects route

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import ProjectDetail from '../views/ProjectDetail.vue'
-import jsonData from '@/assets/data.json'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import ProjectDetail from "@/views/ProjectDetail.vue";
+import jsonData from "@/assets/data.json";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/project/:slug',
-    name: 'ProjectDetail',
-    component: ProjectDetail,
-    props: true
-  }
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/project/:slug",
+        name: "ProjectDetail",
+        component: ProjectDetail,
+        props: true,
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title || "Nabilah Argyanti's Portfolio"
-  next()
-})
+    document.title = to.meta.title || "Nabilah Argyanti's Portfolio";
+    next();
+});
 
 jsonData.projects.forEach((project) => {
-  routes.push({
-    path: `/project/${project.slug}`, // Dynamic route based on project slug
-    name: `Project - ${project.title}`, // Unique name for each project detail route
-    component: ProjectDetail,
-    props: { projectData: project } // Pass project data as props to the component
-  })
-})
+    routes.push({
+        path: `/projects/${project.slug}`,
+        name: `Project - ${project.title}`,
+        component: ProjectDetail,
+        props: { projectData: project },
+    });
+});
 
-export default router
+export default router;
