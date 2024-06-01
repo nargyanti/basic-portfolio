e9dbea273d7452ce9685e3ed91321bc3df4d3c0c
nargyanti <nargyanti@gmail.com>
2024-06-01 21:45:26 +0700

    Change max screen to xl

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+import { RouterView } from "vue-router";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
-<template>
-  <Header></Header>
-  <div class="mx-auto max-w-screen-lg px-6 bg-light md:px-10">
-    <RouterView />
-  </div>
-  <Footer></Footer>
+<template class="">
+    <Header></Header>
+    <div class="bg-light">
+        <div class="mx-auto max-w-screen-xl px-6 md:px-10">
+            <RouterView />
+        </div>
+    </div>
+    <Footer></Footer>
 </template>
