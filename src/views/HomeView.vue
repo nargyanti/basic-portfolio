a12239b474345a08237ffe14a5e8f3bd6b620cbb
nargyanti <nargyanti@gmail.com>
2024-06-13 11:14:01 +0700

    Improve data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,6 @@ import TimelineList from "@/components/TimelineList.vue";
 import data from "@/assets/data.json";
 import SectionTitle from "@/components/SectionTitle.vue";
 import ProfileCard from "@/components/ProfileCard.vue";
-import ProfilePicture from "@/assets/images/profile-picture.webp";
 
 const { profile, education, technical_skills, work_experience, projects } =
     data;
@@ -16,7 +15,7 @@ const { profile, education, technical_skills, work_experience, projects } =
     <section class="bg-light px-6 py-7 md:px-10 md:py-10 lg:py-14">
         <div class="mx-auto flex max-w-screen-xl flex-col items-center gap-6 lg:flex-row-reverse lg:items-start lg:gap-14">
             <!-- Profile Picture::start -->
-            <img :src="ProfilePicture" alt="Profile" class="h-32 rounded-full md:h-56" />
+            <img :src="profile.image" alt="Profile" class="h-32 rounded-full md:h-56" />
             <!-- Profile Picture::end -->
 
             <!-- Bio::start -->
@@ -32,9 +31,8 @@ const { profile, education, technical_skills, work_experience, projects } =
                     <RouterLink :to="{ path: '/', hash: '#contact' }"
                         class="rounded-lg bg-primary px-5 py-3 font-semibold text-white hover:bg-primary-dark">
                         Get in touch</RouterLink>
-                    <a href="/CV - Nabilah Argyanti.pdf" target="_blank" rel="noopener noreferrer"
-                        class="font-semibold text-primary hover:underline">Explore my
-                        resume
+                    <a :href="profile.resume" target="_blank" rel="noopener noreferrer"
+                        class="font-semibold text-primary hover:underline">Explore my resume
                         <Icon icon="heroicons:arrow-top-right-on-square-16-solid" class="mx-1 mb-1 inline" width="20"
                             height="20" />
                     </a>
@@ -61,7 +59,7 @@ const { profile, education, technical_skills, work_experience, projects } =
                 <!-- Skills:start -->
                 <div class="">
                     <SectionTitle class="pb-8">Technical Skills</SectionTitle>
-                    <div v-for="( technical_skill, index ) in  technical_skills " :key="index">
+                    <div v-for="(    technical_skill, index    ) in     technical_skills    " :key="index">
                         <ProfileCard :icon="technical_skill.icon" :title="technical_skill.category"
                             :sub_title="technical_skill.degree" :description="technical_skill.description"
                             :items="technical_skill.skills"
@@ -90,7 +88,7 @@ const { profile, education, technical_skills, work_experience, projects } =
                 problems.</p>
             <div class="flex flex-col items-center pt-8">
                 <div class="grid gap-x-6 gap-y-6 pt-3 md:grid-cols-2 lg:grid-cols-3">
-                    <ProjectCard v-for="( project, index ) in  projects " :key="index" :image="project.image"
+                    <ProjectCard v-for="(    project, index    ) in     projects    " :key="index" :image="project.image"
                         :tags="project.tags" :title="project.title" :description="project.description"
                         :slug="project.slug" />
                 </div>
@@ -113,10 +111,10 @@ const { profile, education, technical_skills, work_experience, projects } =
                 Ready to discuss projects, ideas, or just connect? Feel free to
                 reach out to me
             </p>
-            <a href="mailto:nargyanti@gmail.com" target="_blank" rel="noopener noreferrer"
+            <a :href="`mailto:${profile.email}`" target="_blank" rel="noopener noreferrer"
                 class="mt-4 rounded-lg bg-white px-5 py-3 font-semibold text-primary hover:bg-primary-dark hover:text-white">
                 <Icon icon="material-symbols:mail" class="mr-2 inline" width="30" height="30" />
-                nargyanti@gmail.com
+                {{ profile.email }}
             </a>
         </div>
     </section>
--- src/views/ProjectDetailView.vue
+++ src/views/ProjectDetailView.vue
@@ -21,8 +21,8 @@ function getProjectBySlug(slug) {
 </script>
 
 <template>
-    <div v-if="project" class="pb-20">
-        <section class="bg-light px-6 pt-7 pb-14 md:px-10">
+    <div v-if="project" class="pb-14">
+        <section class="bg-light px-6 pt-7 pb-7 md:pb-14 md:px-10">
             <div class="mb-6">
                 <NavLink :to="{ path: '/', hash: '#projects' }" class="text-primary">
                     <Icon icon="mdi:arrow-left-thin" class="inline mb-1" width="24px" height="24px" />
@@ -30,11 +30,13 @@ function getProjectBySlug(slug) {
                 </NavLink>
             </div>
             <div class="mx-auto flex max-w-screen-xl flex-col lg:flex-row gap-8 lg:gap-14">
-                <img class="max-w-xl object-cover rounded-lg" :src="project.image" :alt="project.title">
+                <div class="max-w-xl overflow-hidden rounded-lg shadow-lg">
+                    <img class="object-cover w-full h-full" :src="project.image" :alt="project.title">
+                </div>
 
-                <div class="flex flex-col gap-5">
-                    <h1 class="text-gray-700 text-3xl font-semibold">{{ project.title }}</h1>
-                    <p>{{ project.description }}</p>
+                <div class="flex flex-col items-center md:items-start gap-5">
+                    <h1 class="text-gray-700 text-3xl font-semibold text-center md:text-start">{{ project.title }}</h1>
+                    <p class="text-gray-600 text-center md:text-start">{{ project.description }}</p>
                     <ItemList :items="project.tools" :column="2" />
                     <div class="flex items-center gap-6 mt-6">
                         <RouterLink :to="{ path: '/', hash: '#contact' }"
@@ -49,22 +51,22 @@ function getProjectBySlug(slug) {
                 </div>
             </div>
         </section>
-        <section class="bg-white px-6 py-7 md:px-10">
+        <section v-if="project.overview" class="bg-white px-6 py-7 md:px-10">
             <div class="mx-auto max-w-screen-xl">
-                <SectionTitle>Overview</SectionTitle>
-                <p class="text-gray-600 pt-4">{{ project.overview }}</p>
+                <SectionTitle class="mb-4">Overview</SectionTitle>
+                <p class="text-gray-600">{{ project.overview }}</p>
             </div>
         </section>
-        <section class="bg-white px-6 py-7 md:px-10">
+        <section v-if="project.features" class="bg-white px-6 py-7 md:px-10">
             <div class="mx-auto max-w-screen-xl">
-                <SectionTitle>Features</SectionTitle>
+                <SectionTitle class="mb-4">Features</SectionTitle>
                 <ItemList :items="project.features" :column="1" />
             </div>
         </section>
-        <section class="bg-white px-6 py-7 md:px-10">
+        <section v-if="project.challenges" class="bg-white px-6 py-7 md:px-10">
             <div class="mx-auto max-w-screen-xl">
-                <SectionTitle>Overview</SectionTitle>
-                <p class="text-gray-600 pt-4">{{ project.challenges }}</p>
+                <SectionTitle class="mb-4">Challenges</SectionTitle>
+                <p class="text-gray-600">{{ project.challenges }}</p>
             </div>
         </section>
     </div>
